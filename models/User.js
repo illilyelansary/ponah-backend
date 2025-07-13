4b6975a23c0279fa7395e8a78538285581a32bf3
illilyelansary <illilyelansary@gmail.com>
2025-07-13 17:51:41 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,26 @@
-role: {
-  type: String,
-  enum: ['admin', 'user'],
-  default: 'user'
-}
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
