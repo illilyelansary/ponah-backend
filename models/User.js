e5985560c485459e0d562d8de815913e8b16c6da
illilyelansary <illilyelansary@gmail.com>
2025-07-13 07:12:13 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,48 +1,5 @@
-// models/User.js
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'  // tous les nouveaux utilisateurs sont des utilisateurs simples par défaut
-  }
-}, { timestamps: true });
-
-// Hash le mot de passe avant sauvegarde
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-
-  try {
-    const salt  = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Méthode pour comparer les mots de passe
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-const User = mongoose.model('User', userSchema);
-export default User;
+role: {
+  type: String,
+  enum: ['admin', 'user'],
+  default: 'user'
+}
